52a2d26aba097ed915b752be18770b5f4a550c5a
Marcelo336 <marcelopintinna@gmail.com>
2026-06-07 15:44:42 -0300

    finished project development

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+import fileRouteConfig from './config/fileRoutes.cjs';
+import './database/index.js';
+import routes from './routes.js';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use('/product-file', fileRouteConfig);
+app.use('/category-file', fileRouteConfig);
+
+app.use(routes);
+
+export default app;
